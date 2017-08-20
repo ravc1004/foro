7d95bf2416011d2c01a688f238e756288318fe21
ravc1004 <ravc1004@gmail.com>
2017-08-20 02:14:30 -0300

    First Commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    
+    use DatabaseTransactions;
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        
+        $user = factory(\App\User::class)->create([
+            'name'=>'Raul Villanueva',
+            'email'=>'ravc1004@gmail.com',
+        ]);
+
+        $this->actingAs($user,'api')
+            ->visit('api/user')
+            ->see('Raul Villanueva')
+            ->see('ravc1004@gmail.com');
+    }
+}
